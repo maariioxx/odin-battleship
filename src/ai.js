226675973c9662c2255ce923238236ed659ec6ad
mario <mariorb2017@gmail.com>
2024-01-03 17:41:42 +0100

    Create AI class

---
+++ src/ai.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/extensions */
+/* eslint-disable class-methods-use-this */
+import isEqual from 'lodash/isEqual.js';
+
+import Player from './player.js';
+
+class AI extends Player {
+  checkHits(enemy) {
+    return enemy.gameboard.hits.concat(enemy.gameboard.missedhits);
+  }
+
+  generateRandomAttack(enemy) {
+    let attacked = false;
+    while (!attacked) {
+      const x = Math.floor(Math.random() * 10);
+      const y = Math.floor(Math.random() * 10);
+      if (this.checkHits(enemy).length > 0) {
+        for (const hit of this.checkHits(enemy)) {
+          if (!isEqual(hit, [x, y])) {
+            enemy.gameboard.receiveAttack(x, y);
+            attacked = true;
+            break;
+          }
+        }
+      } else {
+        enemy.gameboard.receiveAttack(x, y);
+        attacked = true;
+      }
+    }
+  }
+}
+
+export default AI;
