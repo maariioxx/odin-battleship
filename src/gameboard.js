642b9539c59fceb20dd021dbc6eda03201aa841f
mario <mariorb2017@gmail.com>
2024-01-03 17:42:41 +0100

    Finish checkShipList method

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,4 +1,7 @@
-import Ship from './ship.js';
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable class-methods-use-this */
+import isEqual from 'lodash/isEqual.js';
 
 class Gameboard {
   constructor() {
@@ -7,6 +10,16 @@ class Gameboard {
       .map(() => Array(10).fill(0));
     this.ships = [];
     this.hits = [];
+    this.missedhits = [];
+    this.sunkShips = 0;
+  }
+
+  establishShipCoordinates(length, x, y) {
+    const coordinates = [];
+    for (let i = y; i < length + y && i < 9; i++) {
+      coordinates.push([x, i]);
+    }
+    return coordinates;
   }
 
   placeShip(ship, x, y) {
@@ -15,36 +28,45 @@ class Gameboard {
       this.grid[x][i] = 1;
     }
     this.ships.push({
-      ship: ship,
-      x: x,
-      y: y,
+      ship,
+      coordinates: this.establishShipCoordinates(ship.length, x, y),
     });
   }
 
   checkShipList(x, y) {
-    for (const [key, value] of Object.entries(this.ships)) {
-      if (value.x === x && value.y === y) return value.ship;
-    }
+    let equal;
+    Object.values(this.ships).forEach((value) => {
+      value.coordinates.forEach((c) => {
+        if (isEqual(c, [x, y])) equal = value.ship;
+      });
+    });
+    return equal;
+  }
+
+  checkIfAllShipsSunked() {
+    return this.ships.length === this.sunkShips;
   }
 
   receiveAttack(x, y) {
     if (this.grid[x][y] === 0) {
-      this.hits.push([x, y]);
+      this.missedhits.push([x, y]);
       return false;
     } else if (this.grid[x][y] === 2) {
       return false;
     } else {
       this.hits.push([x, y]);
+
       const hitShip = this.checkShipList(x, y);
       hitShip.hitNumber += 1;
-      hitShip.isSunk();
+      this.grid[x][y] = 2;
+      if (hitShip.isSunk()) {
+        this.sunkShips += 1;
+        hitShip.sunk = true;
+      }
+      if (this.checkIfAllShipsSunked()) return 'All ships sunked';
       return hitShip.hitNumber;
     }
   }
 }
 
-const gameboard = new Gameboard();
-const ship = new Ship(4);
-gameboard.placeShip(ship, 5, 3);
-console.log(gameboard.receiveAttack(5, 3));
 export default Gameboard;
