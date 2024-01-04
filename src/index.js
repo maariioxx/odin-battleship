799aa46e3edaf3dcc5950be1f19afcd0325378d9
mario <mariorb2017@gmail.com>
2024-01-04 11:28:44 +0100

    Create grids when page loaded

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+/* eslint-disable no-undef */
+/* eslint-disable import/extensions */
 import './styles.css';
+import { createGrid } from './DOM.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  createGrid('player');
+  createGrid('AI');
+});
