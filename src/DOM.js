43d7404163dd679f72885c09ea3dd6bc66085c13
mario <mariorb2017@gmail.com>
2024-01-05 21:48:16 +0100

    Create show winner method

--- src/DOM.js
+++ src/DOM.js
@@ -1,14 +1,16 @@
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable no-undef */
+const message = document.querySelector('.message');
 const grid = document.querySelector('.grid');
 const AIgrid = document.querySelector('.aigrid');
+const changeDirectionMessage = document.querySelector('.direction-message');
 
-export const createGrid = (playerOrAI) => {
+const createGrid = (playerOrAI) => {
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       const cell = document.createElement('div');
       cell.classList.add('cell');
       cell.style.border = '1px solid black';
+      cell.dataset.x = j;
+      cell.dataset.y = i;
       cell.style.width = '40px';
       cell.style.height = '40px';
       if (playerOrAI === 'player') {
@@ -21,3 +23,33 @@ export const createGrid = (playerOrAI) => {
     }
   }
 };
+
+const displayShips = (DOMgrid, gameboardGrid, AI) => {
+  for (let i = 0; i < DOMgrid.length; i++) {
+    const { x } = DOMgrid[i].dataset;
+    const { y } = DOMgrid[i].dataset;
+    if (gameboardGrid[x][y] === 1 && !AI)
+      DOMgrid[i].style.backgroundColor = 'lightblue';
+    else if (gameboardGrid[x][y] === 2)
+      DOMgrid[i].style.backgroundColor = 'red';
+    else if (gameboardGrid[x][y] === 4)
+      DOMgrid[i].style.backgroundColor = 'gray';
+  }
+};
+
+const startGameText = () => {
+  changeDirectionMessage.remove();
+  if (!document.querySelector('.game-message')) {
+    const gameStarted = document.createElement('p');
+    gameStarted.classList.add('game-message');
+    gameStarted.textContent = 'Game started!';
+    message.appendChild(gameStarted);
+  }
+};
+
+const showWinner = (winner) => {
+  const gameMessage = document.querySelector('.game-message');
+  gameMessage.textContent = `${winner} wins!`;
+};
+
+export { createGrid, displayShips, startGameText, showWinner };
