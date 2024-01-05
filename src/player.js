0f88d2ad143e48d35f56c3b9899b1a314151893c
mario <mariorb2017@gmail.com>
2024-01-05 21:49:32 +0100

    Finish project

--- src/player.js
+++ src/player.js
@@ -1,15 +1,75 @@
-/* eslint-disable import/extensions */
-/* eslint-disable class-methods-use-this */
 import Gameboard from './gameboard.js';
+import Ship from './ship.js';
 
 class Player {
   constructor() {
     this.gameboard = new Gameboard();
-    this.turn = true;
   }
 
   attack(enemy, x, y) {
     enemy.gameboard.receiveAttack(x, y);
+  }
+
+  placePlayerShips(cell, playercells, length, d) {
+    if (d === 'v') {
+      for (
+        let i = cell.dataset.y;
+        i - cell.dataset.y < length && cell.dataset.y < 11 - length;
+        i++
+      ) {
+        playercells[Number(`${i}${cell.dataset.x}`)].style.backgroundColor =
+          'lightblue';
+        cell.addEventListener('mouseleave', () => {
+          for (
+            let j = cell.dataset.y;
+            j - cell.dataset.y < length && cell.dataset.y < 11 - length;
+            j++
+          ) {
+            if (this.gameboard.grid[cell.dataset.x][j] === 0)
+              playercells[
+                Number(`${j}${cell.dataset.x}`)
+              ].style.backgroundColor = `#161616`;
+          }
+        });
+      }
+      cell.addEventListener('click', () => {
+        this.gameboard.placeShip(
+          new Ship(length),
+          Number(cell.dataset.x),
+          Number(cell.dataset.y),
+          'v'
+        );
+      });
+    } else {
+      for (
+        let i = cell.dataset.x;
+        i - cell.dataset.x < length && cell.dataset.x < 11 - length;
+        i++
+      ) {
+        playercells[Number(`${cell.dataset.y}${i}`)].style.backgroundColor =
+          'lightblue';
+        cell.addEventListener('mouseleave', () => {
+          for (
+            let j = cell.dataset.x;
+            j - cell.dataset.x < length && cell.dataset.x < 11 - length;
+            j++
+          ) {
+            if (this.gameboard.grid[j][cell.dataset.y] === 0)
+              playercells[
+                Number(`${cell.dataset.y}${j}`)
+              ].style.backgroundColor = 'white';
+          }
+        });
+      }
+      cell.addEventListener('click', () => {
+        this.gameboard.placeShip(
+          new Ship(length),
+          Number(cell.dataset.x),
+          Number(cell.dataset.y),
+          'h'
+        );
+      });
+    }
   }
 }
 
